8943b0d17911cfc81206d6e4f9f1b6eca56c64a0
Dave Buchanan <dave@joesvolcano.net>
2026-02-27 17:20:17 -0800

    a

--- packages/vite-express-prisma-adminjs/.adminjs/entry.js
+++ packages/vite-express-prisma-adminjs/.adminjs/entry.js
@@ -1,1 +1,5 @@
 AdminJS.UserComponents = {}
+import ManageDealContacts from '../api/admin/ManageDealContacts'
+AdminJS.UserComponents.ManageDealContacts = ManageDealContacts
+import ManageContactDeals from '../api/admin/ManageContactDeals'
+AdminJS.UserComponents.ManageContactDeals = ManageContactDeals
